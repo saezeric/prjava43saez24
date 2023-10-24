5531a93dbd02cc75f00c0e95db3269c2eb577b32
saezeric <saezescalonaeric@fpllefia.com>
2023-10-24 16:29:01 +0200

    Versió 0.2-A del proyecte prjava43saez24. Branca00 creada i modificada.

--- NetBeansProjects/prjava43saez24/src/prjava43saez24/Prjava43saez24.java
+++ NetBeansProjects/prjava43saez24/src/prjava43saez24/Prjava43saez24.java
@@ -1,6 +1,7 @@
 package prjava43saez24;
 import java.awt.*;        // using AWT containers and components 
-import java.awt.event.*;  // using AWT events and listener interfaces 
+import java.awt.event.*;  // using AWT events and listener interfaces
+import java.net.*;
  
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava43saez24 extends Frame 
